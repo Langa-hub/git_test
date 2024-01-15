e1425a5e884b1b0a69f4b4ae63defb2c03a45e02
Langa-hub <langakabonjojospin@gmail.com>
2024-01-15 15:02:03 +0100

    change app name

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Jospin-Hub',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
